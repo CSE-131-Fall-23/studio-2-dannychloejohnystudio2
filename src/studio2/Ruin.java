9cf75f75960a07da9b970577377140c46622c283
dyoo27 <dcyoo28@gmail.com>
2023-09-14 10:12:21 -0500

    h

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,23 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("The amount of money that you start with: ");
+		double startAmount = in.nextDouble();
+		System.out.print("The win probability(single play): ");
+		double winChance = in.nextDouble();
+		System.out.print("The win limit: ");
+		double winLimit = in.nextDouble();
+		
+		
+		
+		
+	}
+
+}
