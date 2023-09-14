e381db15b3f6704ef3f1030cdc9f34b2e926d53e
johnyeskandar <johnyeskandar@localhost>
2023-09-14 11:20:23 -0500

    pi

--- src/studio2/Pi.java
+++ src/studio2/Pi.java
@@ -1,9 +1,24 @@
 package studio2;
 
+import java.util.Scanner;
+
 public class Pi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+			int numdarts = in.nextInt();
+     double x = Math.random();
+     double y = Math.random();
+     int hit = 0;
+     for(int i=0; i <= numdarts; i++)
+     {
+    	 if(Math.sqrt(x*x + y*y) <= 1);
+	     {
+	    	 hit++;
+	     }
+     }
+	     
 
 	}
 
